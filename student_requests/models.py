ca850ebc801d511ed0cd6508d00ea1d98352dd9b
fahimasogol <fahimasogol@gmail.com>
2023-10-24 13:13:45 +0330

    added the models for requests

--- student_requests/models.py
+++ student_requests/models.py
@@ -1,37 +1,40 @@
 from django.db import models
+# from courses.models import *
+# from users.models import *
+
 
 # Create your models here.
 class CourseRegistrationRequest(models.Model):
-    student = models.ForeignKey('Student', on_delete=models.CASCADE)
-    requested_courses = models.ManyToManyField('CourseTerm')
+    student = models.ForeignKey('users.Student', on_delete=models.CASCADE)
+    requested_courses = models.ManyToManyField('courses.CourseTerm')
     approval_status = models.BooleanField(default=False)
 
 
 class CourseCorrectionRequest(models.Model):
-    student = models.ForeignKey('Student', on_delete=models.CASCADE)
-    courses_to_drop = models.ManyToManyField('CourseTerm', related_name='drop_requests')
-    courses_to_add = models.ManyToManyField('CourseTerm', related_name='add_requests')
+    student = models.ForeignKey('users.Student', on_delete=models.CASCADE)
+    courses_to_drop = models.ManyToManyField('courses.CourseTerm', related_name='drop_requests')
+    courses_to_add = models.ManyToManyField('courses.CourseTerm', related_name='add_requests')
     approval_status = models.BooleanField(default=False)
 
 
 class GradeReconsiderationRequest(models.Model):
-    student = models.ForeignKey('Student', on_delete=models.CASCADE)
-    course = models.ForeignKey('CourseTerm', on_delete=models.CASCADE)
+    student = models.ForeignKey('users.Student', on_delete=models.CASCADE)
+    course = models.ForeignKey('courses.CourseTerm', on_delete=models.CASCADE)
     reconsideration_text = models.TextField()
     response_text = models.TextField(blank=True, null=True)
 
 
 class EmergencyDropRequest(models.Model):
-    student = models.ForeignKey('Student', on_delete=models.CASCADE)
-    course = models.ForeignKey('CourseTerm', on_delete=models.CASCADE)
+    student = models.ForeignKey('users.Student', on_delete=models.CASCADE)
+    course = models.ForeignKey('courses.CourseTerm', on_delete=models.CASCADE)
     result = models.BooleanField(default=False)
     student_comment = models.TextField()
     deputy_educational_comment = models.TextField(blank=True, null=True)
 
 
 class TermDropRequest(models.Model):
-    student = models.ForeignKey('Student', on_delete=models.CASCADE)
-    term = models.ForeignKey('Term', on_delete=models.CASCADE)
+    student = models.ForeignKey('users.Student', on_delete=models.CASCADE)
+    term = models.ForeignKey('courses.Term', on_delete=models.CASCADE)
     result = models.CharField(max_length=50, choices=[('With Seniority', 'With Seniority'),
                                                       ('Without Seniority', 'Without Seniority')])
     student_comment = models.TextField()
@@ -39,7 +42,7 @@ class TermDropRequest(models.Model):
 
 
 class MilitaryServiceRequest(models.Model):
-    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    student = models.ForeignKey('users.Student', on_delete=models.CASCADE)
     proof_document = models.FileField(upload_to='military_docs/')
-    term = models.ForeignKey('Term', on_delete=models.CASCADE)
+    term = models.ForeignKey('courses.Term', on_delete=models.CASCADE)
     issuance_place = models.CharField(max_length=100)
