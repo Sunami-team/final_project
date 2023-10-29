00c77d3f3a8e57da0c311fdf184aa849c29a8d8e
mahdi <mohammodmahdi614@gmail.com>
2023-10-29 22:30:12 +0330

    edit view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import serializers
 
-# Create your views here.
+from users.models import DeputyEducational
+
+
+class AssistanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DeputyEducational
+        field = '__all__'
+
+
+class AssistanList(generics.ListAPIView):
+    queryset = DeputyEducational.objects.all()
+    serializer_class = AssistanSerializer
+
+
+class AssistanDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = DeputyEducational.objects.all()
+    serializer_class = AssistanSerializer
+
+
+class AssistanCreate(generics.CreateAPIView):
+    queryset = DeputyEducational.objects.all()
+    serializer_class = AssistanSerializer
+
+
+class AssistanUpdate(generics.UpdateAPIView):
+    queryset = DeputyEducational.objects.all()
+    serializer_class = AssistanSerializer
+
+
+class AssistanDelete(generics.DestroyAPIView):
+    queryset = DeputyEducational.objects.all()
+    serializer_class = AssistanSerializer
