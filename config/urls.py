a16daf9f1c602e992f031986d10b999f0cce36b0
sinahs1992 <sinahs1992@gmail.com>
2023-10-30 23:28:55 +0330

    resolve swagger branch

--- config/urls.py
+++ config/urls.py
@@ -36,4 +36,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/users/', include('users.urls', namespace='v1')),
+    # swagger and redoc urls
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
