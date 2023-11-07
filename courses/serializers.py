d7536a2d0463693707d4c9ce752d7db735af0129
mahdi <mohammodmahdi614@gmail.com>
2023-11-07 23:25:48 +0330

    ADD selection course

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from courses.models import Term
+from courses.models import Term, StudentCourse
 
 
 class TermSerializer(serializers.ModelSerializer):
     class Meta:
         model = Term
+        fields = '__all__'
+
+
+class CourseSelectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudentCourse
         fields = '__all__'
--- courses/views.py
+++ courses/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import viewsets
 from rest_framework.generics import RetrieveAPIView
 
 from courses.models import Term
 from courses.serializers import TermSerializer
+
+from rest_framework import generics
+from .serializers import CourseSelectionSerializer
 
 
 class TermViewSet(viewsets.ModelViewSet):
@@ -16,3 +16,16 @@ class TermViewSet(viewsets.ModelViewSet):
 class TermDetailAPIView(RetrieveAPIView):
     queryset = Term.objects.all()
     serializer_class = TermSerializer
+
+
+class CourseSelectionCreateView(generics.CreateAPIView):
+    serializer_class = CourseSelectionSerializer
+
+
+def create(self, request, *args, **kwargs):
+    # بررسی زمان شروع و پایان انتخاب واحد
+    # بررسی تعداد واحدهای حداکثر و حداقل
+    # بررسی شرایط پیش‌نیازی
+    # بررسی وضعیت دروس
+    # ثبت انتخاب واحد
+    return super().create(request, *args, **kwargs)
