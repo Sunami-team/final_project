a4ac34b366664bb455f4bf13b201c0483dd60185
mahdi <mohammodmahdi614@gmail.com>
2023-11-07 22:51:53 +0330

    add term ditail

--- courses/views.py
+++ courses/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
+from rest_framework.generics import RetrieveAPIView
 
 from courses.models import Term
 from courses.serializers import TermSerializer
 
 
 class TermViewSet(viewsets.ModelViewSet):
+    queryset = Term.objects.all()
+    serializer_class = TermSerializer
+
+
+class TermDetailAPIView(RetrieveAPIView):
     queryset = Term.objects.all()
     serializer_class = TermSerializer
