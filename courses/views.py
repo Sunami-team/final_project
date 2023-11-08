1681d683a345fb107c91de03ec8ad315668a07c7
mahdi <mohammodmahdi614@gmail.com>
2023-11-08 22:42:36 +0330

    ADD selection course

--- courses/views.py
+++ courses/views.py
@@ -21,11 +21,10 @@ class TermDetailAPIView(RetrieveAPIView):
 class CourseSelectionCreateView(generics.CreateAPIView):
     serializer_class = CourseSelectionSerializer
 
-
-def create(self, request, *args, **kwargs):
-    # بررسی زمان شروع و پایان انتخاب واحد
-    # بررسی تعداد واحدهای حداکثر و حداقل
-    # بررسی شرایط پیش‌نیازی
-    # بررسی وضعیت دروس
-    # ثبت انتخاب واحد
-    return super().create(request, *args, **kwargs)
+    def create(self, request, *args, **kwargs):
+        # بررسی زمان شروع و پایان انتخاب واحد
+        # بررسی تعداد واحدهای حداکثر و حداقل
+        # بررسی شرایط پیش‌نیازی
+        # بررسی وضعیت دروس
+        # ثبت انتخاب واحد
+        return super().create(request, *args, **kwargs)
