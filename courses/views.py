a93f4ec47039be3132dd8bacc76acbe9813aec6d
mahdi <mohammodmahdi614@gmail.com>
2023-11-09 17:03:47 +0330

    add condition

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,6 @@
+
+from django.db.models import Sum
+from django.utils import timezone
 from rest_framework import viewsets
 from rest_framework.generics import RetrieveAPIView
 
@@ -27,14 +30,50 @@ class StudentViewSet(viewsets.ModelViewSet):
     def create_course_selection(self, request, pk=None):
         student = self.get_object()
         serializer = CourseSelectionSerializer(data=request.data)
+
         # بررسی زمان شروع و پایان انتخاب واحد
+        term = Term.objects.get(name='ترم جاری')  # ترم جاری را بر اساس منطق برنامه شما باید انتخاب کنید
+        if not (term.registration_start_datetime <= timezone.now() <= term.registration_end_datetime):
+            return Response({'error': 'زمان انتخاب واحد به پایان رسیده است.'}, status=status.HTTP_400_BAD_REQUEST)
+
         # بررسی تعداد واحدهای حداکثر و حداقل
+        min_credit_units = 12  # حداقل تعداد واحدهای مجاز
+        max_credit_units = 20  # حداکثر تعداد واحدهای مجاز
+        selected_units = request.data.get('units', 0)
+        if not (min_credit_units <= selected_units <= max_credit_units):
+            return Response({'error': 'تعداد واحدهای انتخاب شده نامعتبر است.'}, status=status.HTTP_400_BAD_REQUEST)
+
         # بررسی شرایط پیش‌نیازی
+        prerequisites = request.data.get('prerequisites', [])
+        for prerequisite_id in prerequisites:
+            if not student.completed_courses.filter(id=prerequisite_id).exists():
+                return Response({'error': 'شما شرایط پیش‌نیاز را برآورده نکرده‌اید.'},
+                                status=status.HTTP_400_BAD_REQUEST)
+
         # بررسی وضعیت دروس
-        # ثبت انتخاب واحد
+        ongoing_courses = student.ongoing_courses.filter(term=term)
+        if len(ongoing_courses) > 0:
+            return Response({'error': 'شما قبلاً دروسی را در این ترم انتخاب کرده‌اید.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
         if serializer.is_valid():
+            # ثبت انتخاب واحد
             course_selection = serializer.save(student=student)
+
+            # احتساب واحدها
+            student_units = student.ongoing_courses.filter(term=term).aggregate(Sum('credit_units'))[
+                'credit_units__sum']
+            if student_units is None:
+                student_units = 0
+            student_units += selected_units
+            if student_units > max_credit_units:
+                # اگر تعداد واحدهای دانشجو بیشتر از حداکثر مجاز است، انتخاب واحد لغو می‌شود
+                course_selection.delete()
+                return Response({'error': 'تعداد واحدهای انتخاب شده بیشتر از حداکثر مجاز است.'},
+                                status=status.HTTP_400_BAD_REQUEST)
+
             return Response(CourseSelectionSerializer(course_selection).data, status=status.HTTP_201_CREATED)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=True, methods=['get'])
