af1066940f0407e5ce7fcd39efddf065fc53d2c9
Mohi <mohifalahi@gmail.com>
2023-11-11 23:10:11 +0330

    file upload desc to urls.py

--- config/urls.py
+++ config/urls.py
@@ -45,6 +45,6 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
-
+# to access file urls
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
