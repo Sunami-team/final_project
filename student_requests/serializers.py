a912d7b3ebf176065dd5b9696c5c1902055027c9
sinahs1992 <sinahs1992@gmail.com>
2023-11-12 15:34:12 +0330

    add accept remove term with assistant

--- student_requests/serializers.py
+++ student_requests/serializers.py
@@ -1,8 +1,8 @@
-from .models import Course, CourseTerm
+from courses.models import Course, CourseTerm
 from rest_framework import serializers
+from .models import TermDropRequest
 
-
-class CourseSerializer(serializers.ModelSerilaizer):
+class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ['name', 'faculty', 'pre_requisites', 'co_requisites', 'course_unit', 'course_type']
@@ -19,3 +19,14 @@ class CourseTermSerializer(serializers.ModelSerializer):
             'exam_date_time',
             'exam_location'
         ]
+
+class TermDropSerializer(serializers.ModelSerializer):
+    student_first_name = serializers.CharField(source='student.first_name', read_only=True)
+    student_last_name = serializers.CharField(source='student.last_name', read_only=True)
+    student_id = serializers.CharField(source='student.id', read_only=True)
+    term_name = serializers.CharField(source='term.name', read_only=True)
+    accept = serializers.BooleanField(default=False, write_only=True)
+    class Meta:
+        model = TermDropRequest
+        fields = ('student_id', 'student_first_name', 'student_last_name', 'term_name', 'result', 'student_comment', 'deputy_educational_comment', 'accept')
+        read_only_fields = ('student_id', 'student_first_name', 'student_last_name', 'term_name', 'result', 'student_comment')
