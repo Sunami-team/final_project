43ec645c14f68dee5ccce3b506c7e7a981b6fc7b
Mohi <mohifalahi@gmail.com>
2023-11-13 09:21:35 +0330

    post scores added

--- courses/urls.py
+++ courses/urls.py
@@ -1,3 +1,6 @@
+from django.url import path
+from . import views
+
 urlpatterns = [
-    
+    ('professor/<int:pk>/coures/<int:c_pk>/scores/', views.PostScoresApiView.as_view(), name="post-scores"),
 ]
