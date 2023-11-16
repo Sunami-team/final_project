b3bda1cf8526307318219fe0165fab5bb9624a40
masoud <masoudfakkary@gmail.com>
2023-11-16 10:31:28 +0330

    Implement APIs for militery service request

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'files') # This defines the absolute path for uploads
 MEDIA_URL = '/media/'
