a8ad3194534a3634121284ca4eaf2e4324df4e51
Mohi <mohifalahi@gmail.com>
2023-11-23 00:00:07 +0330

    static_root and collectstatic

--- config/settings.py
+++ config/settings.py
@@ -171,6 +171,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(
     BASE_DIR, "files"
